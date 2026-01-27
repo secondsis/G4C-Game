e34355af447015c709027028c027905271eacd3c
secondsis <lookatmyflames@gmail.com>
2026-01-26 22:46:32 -0500

    new files

---
+++ Assets/Main/Scripts/SFXManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager Instance;
+    [SerializeField] private AudioSource buySfx;
+    [SerializeField] private AudioSource harvestSfx;
+    [SerializeField] private AudioSource sellSfx;
+    [SerializeField] private AudioSource shopEnterSfx;
+
+    private void Awake()
+    {
+        
+        // THIS IS A SINGLETON
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+    
+    public void BuySfx()
+    {
+        buySfx.Play();
+    }
+
+    public void SellSfx()
+    {
+        sellSfx.Play();
+    }
+
+    public void HarvestSfx()
+    {
+        harvestSfx.Play();
+    }
+
+    public void ShopEnterSfx()
+    {
+        shopEnterSfx.time = 2f;
+        shopEnterSfx.Play();
+    }
+}
