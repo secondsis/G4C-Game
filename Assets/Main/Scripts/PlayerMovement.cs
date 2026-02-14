e8af7b1bf9167d12a0c8284cbe56a345cc180025
secondsis <lookatmyflames@gmail.com>
2026-02-14 11:52:00 -0500

    PLAYERMOVEMENT OVERHAUL DONE (probably) it still feels a bit space-y

--- Assets/Main/Scripts/PlayerMovement.cs
+++ Assets/Main/Scripts/PlayerMovement.cs
@@ -108,78 +108,6 @@ public class PlayerMovement : MonoBehaviour
             PlayIdleAnim();
         }
     }
-    
-    //     private void Update()
-    // {
-    //     checkShiftlock();
-    //     Vector3 forward = playerCamera.transform.TransformDirection(Vector3.forward);
-    //     Vector3 right = playerCamera.transform.TransformDirection(Vector3.right);
-    //
-    //     bool isRunning = Input.GetKey(KeyCode.LeftShift);
-    //     float curSpeedX = _canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-    //     float curSpeedY = _canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-    //     float movementDirectionY = _moveDirection.y;
-    //     if(curSpeedX != 0 && curSpeedY != 0)
-    //         _moveDirection = (forward * curSpeedX + (right * curSpeedY)) / 1.41421356237f;
-    //     else _moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-    //
-    //     if (curSpeedX == 0 && curSpeedY == 0)
-    //     {
-    //         this._isRunning = false;
-    //     }
-    //     else
-    //     {
-    //         this._isRunning = true;
-    //     }
-    //
-    //     if (_characterController.isGrounded)
-    //     {
-    //         _isJumping = false;
-    //     }
-    //
-    //     if (Input.GetButton("Jump") && _canMove && _characterController.isGrounded)
-    //     {
-    //         _moveDirection.y = jumpPower;
-    //         _isJumping = true;
-    //         PlayJumpAnim();
-    //     }
-    //     else
-    //     {
-    //         _moveDirection.y = movementDirectionY;
-    //     }
-    //
-    //     if (!_characterController.isGrounded)
-    //     {
-    //         _moveDirection.y -= gravity * Time.deltaTime;
-    //     }
-    //
-    //     if (Input.GetKey(KeyCode.R) && _canMove)
-    //     {
-    //         _characterController.height = crouchHeight;
-    //         walkSpeed = crouchSpeed;
-    //         runSpeed = crouchSpeed;
-    //
-    //     }
-    //     else
-    //     {
-    //         _characterController.height = defaultHeight;
-    //         walkSpeed = 6f;
-    //         runSpeed = 12f;
-    //     }
-    //
-    //     _characterController.Move(_moveDirection * Time.deltaTime);
-    //
-    //     if (_canMove)
-    //     {
-    //         _rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-    //         _rotationX = Mathf.Clamp(_rotationX, -lookXLimit, lookXLimit);
-    //         playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
-    //         playerCamera.transform.LookAt(transform);
-    //         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-    //     }
-    //
-    //     manageAnimations();
-    // }
 
     // Problem: 1. Player changes directions instantly  2. Player's directions are inaccurate
     private void Update()
@@ -258,77 +186,40 @@ public class PlayerMovement : MonoBehaviour
             _camRotationY = Mathf.Clamp(_camRotationY, -lookYLimit, lookYLimit);
 
             cameraPivot.transform.localRotation = Quaternion.Euler(0, _camRotationX, _camRotationY);
-
-            Quaternion temp = Quaternion.identity;
+            
             // Need to calculate _playerRotationY based on camera's current rotation
             if (Input.GetAxis("Horizontal") >= 0.1f)
             {
-                _playerRotation = Quaternion.LookRotation(cameraPivot.forward, Vector3.up);
-                temp = Quaternion.LookRotation(cameraPivot.forward, Vector3.up);
+                _playerRotation = Quaternion.LookRotation(cameraPivot.forward *-1, Vector3.up);
                 DebugScript.BetterDebug(Quaternion.LookRotation(cameraPivot.forward, Vector3.up));
             } else if (Input.GetAxis("Horizontal") <= -0.1f)
             {
-                _playerRotation = -Quaternion.LookRotation(cameraPivot.forward).y;
-                temp = Quaternion.LookRotation(cameraPivot.forward * -1, Vector3.up);
-            }
-
-            if (Input.GetAxis("Vertical") >= 0.1f)
+                _playerRotation = Quaternion.LookRotation(cameraPivot.forward, Vector3.up);
+            } else if (Input.GetAxis("Vertical") >= 0.1f)
             {
-                _playerRotation = 90f;
+                _playerRotation = Quaternion.LookRotation(cameraPivot.right, Vector3.up);
             } else if (Input.GetAxis("Vertical") <= -0.1f)
             {
-                _playerRotation = 270f;
+                _playerRotation = Quaternion.LookRotation(cameraPivot.right * -1, Vector3.up);
             }
-
+            
             // Idk any better way to do it off the top of my mind
             if (Input.GetAxis("Horizontal") >= 0.1f && Input.GetAxis("Vertical") >= 0.1f)
             {
-                _playerRotation = 135f;
+                _playerRotation = Quaternion.LookRotation(cameraPivot.right + (cameraPivot.forward * -1), Vector3.up);
             } else if (Input.GetAxis("Horizontal") >= 0.1f && Input.GetAxis("Vertical") <= -0.1f)
             {
-                _playerRotation = 225f;
+                _playerRotation = Quaternion.LookRotation((-1 * cameraPivot.right) + (cameraPivot.forward * -1), Vector3.up);
             } else if (Input.GetAxis("Horizontal") <= -0.1f && Input.GetAxis("Vertical") >= 0.1f)
             {
-                _playerRotation = 45f;
+                _playerRotation = Quaternion.LookRotation(cameraPivot.right + (cameraPivot.forward), Vector3.up);
             } else if (Input.GetAxis("Horizontal") <= -0.1f && Input.GetAxis("Vertical") <= -0.1f)
             {
-                _playerRotation = 315f;
+                _playerRotation = Quaternion.LookRotation((cameraPivot.right * -1) + (cameraPivot.forward), Vector3.up);
             }
             
-            // if (Input.GetAxis("Horizontal") >= 0.1f)
-            // {
-            //     _playerRotationY = 180f;
-            // } else if (Input.GetAxis("Horizontal") <= -0.1f)
-            // {
-            //     _playerRotationY = 0f;
-            // }
-            //
-            // if (Input.GetAxis("Vertical") >= 0.1f)
-            // {
-            //     _playerRotationY = 90f;
-            // } else if (Input.GetAxis("Vertical") <= -0.1f)
-            // {
-            //     _playerRotationY = 270f;
-            // }
-            //
-            // // Idk any better way to do it off the top of my mind
-            // if (Input.GetAxis("Horizontal") >= 0.1f && Input.GetAxis("Vertical") >= 0.1f)
-            // {
-            //     _playerRotationY = 135f;
-            // } else if (Input.GetAxis("Horizontal") >= 0.1f && Input.GetAxis("Vertical") <= -0.1f)
-            // {
-            //     _playerRotationY = 225f;
-            // } else if (Input.GetAxis("Horizontal") <= -0.1f && Input.GetAxis("Vertical") >= 0.1f)
-            // {
-            //     _playerRotationY = 45f;
-            // } else if (Input.GetAxis("Horizontal") <= -0.1f && Input.GetAxis("Vertical") <= -0.1f)
-            // {
-            //     _playerRotationY = 315f;
-            // }
-            
             // Spherical lerp (treats vectors as directions instead of positions) what the physics
-            // characterTransform.rotation = Quaternion.Slerp(characterTransform.rotation, Quaternion.AngleAxis(_playerRotationY, Vector3.up), Time.deltaTime * 10f);
-            characterTransform.rotation = Quaternion.Slerp(characterTransform.rotation, temp, Time.deltaTime * 10f);
+            characterTransform.rotation = Quaternion.Slerp(characterTransform.rotation,_playerRotation, Time.deltaTime * 10f);
         }
 
         manageAnimations();
